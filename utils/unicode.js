17b6575064f76860c076571da25d6821ef64af26
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2024-06-01 17:37:51 -0400

    comments

--- utils/unicode.js
+++ utils/unicode.js
@@ -1,3 +1,4 @@
+// Translate emoji to unicode
 function getUnicode(text) {
     const regex = /(\p{Emoji_Presentation}|\p{Emoji}\uFE0F)/gu;
     const emojis = text.match(regex);
@@ -7,6 +8,7 @@ function getUnicode(text) {
     return null;
 }
 
+// Translate unicode to emoji
 function getEmoji(unicode) {
     const codePoints = unicode.split('U+').slice(1).map(point => parseInt(point, 16));
     const emojis = String.fromCodePoint(...codePoints);
